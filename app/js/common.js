69e15c2d17406c3255beb2084c450ae9186c5dbf
odinokun <odinokun@gmail.com>
2016-07-07 21:30:00 +0300

    create tab__section

--- app/js/common.js
+++ app/js/common.js
@@ -1,3 +1,16 @@
 $(function() {
-    console.log("Hasta la vista, baby");
-})
+    // console.log("Hasta la vista, baby");
+    $('.tab__control-link').on('click', function(e){
+        e.preventDefault();
+
+        var item = $(this).closest('.tab__controls-item'),
+            contentItem = $('.tab__item'),
+            itemPosition = item.data('class');
+
+        contentItem.filter('.tab__item_' + itemPosition)
+            .add(item)
+            .addClass('active')
+            .siblings()
+            .removeClass('active');
+    });
+});
