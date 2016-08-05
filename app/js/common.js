324003c5e0c2c8ed653910527b39e54513ea7320
odinokun <odinokun@gmail.com>
2016-08-05 12:04:18 +0300

    edit BXslider pager controls

--- app/js/common.js
+++ app/js/common.js
@@ -146,92 +146,92 @@ $(function() {
 });
 
 
-// bx slider 01 in top__section
-$(document).ready(function() {
-    $('#top__slider-01').bxSlider({
-        mode: 'fade',
-        buildPager: function(slideIndex) {
-            switch (slideIndex) {
-                case 0:
-                    return 'сайт-визитка';
-                case 1:
-                    return 'одностраничный сайт';
-                case 2:
-                    return 'корпоративный сайт';
-                case 3:
-                    return 'сайт-каталог';
-                case 4:
-                    return 'интернет-магазин';
-            }
-        }
-    });
-});
+// // bx slider 01 in top__section
+// $(document).ready(function() {
+//     $('#top__slider-01').bxSlider({
+//         mode: 'fade',
+//         buildPager: function(slideIndex) {
+//             switch (slideIndex) {
+//                 case 0:
+//                     return 'сайт-визитка';
+//                 case 1:
+//                     return 'одностраничный сайт';
+//                 case 2:
+//                     return 'корпоративный сайт';
+//                 case 3:
+//                     return 'сайт-каталог';
+//                 case 4:
+//                     return 'интернет-магазин';
+//             }
+//         }
+//     });
+// });
 
 
-// bx slider 02 in top__section
-$(document).ready(function() {
-    $('#top__slider-02').bxSlider({
-        mode: 'fade',
-        buildPager: function(slideIndex) {
-            switch (slideIndex) {
-                case 0:
-                    return 'SEO';
-                case 1:
-                    return 'одностраничный сайт';
-                case 2:
-                    return 'корпоративный сайт';
-                case 3:
-                    return 'сайт-каталог';
-                case 4:
-                    return 'интернет-магазин';
-            }
-        }
-    });
-});
+// // bx slider 02 in top__section
+// $(document).ready(function() {
+//     $('#top__slider-02').bxSlider({
+//         mode: 'fade',
+//         buildPager: function(slideIndex) {
+//             switch (slideIndex) {
+//                 case 0:
+//                     return 'SEO';
+//                 case 1:
+//                     return 'одностраничный сайт';
+//                 case 2:
+//                     return 'корпоративный сайт';
+//                 case 3:
+//                     return 'сайт-каталог';
+//                 case 4:
+//                     return 'интернет-магазин';
+//             }
+//         }
+//     });
+// });
 
 
-// bx slider 03 in top__section
-$(document).ready(function() {
-    $('#top__slider-03').bxSlider({
-        mode: 'fade',
-        buildPager: function(slideIndex) {
-            switch (slideIndex) {
-                case 0:
-                    return 'дизайн';
-                case 1:
-                    return 'одностраничный сайт';
-                case 2:
-                    return 'корпоративный сайт';
-                case 3:
-                    return 'сайт-каталог';
-                case 4:
-                    return 'интернет-магазин';
-            }
-        }
-    });
-});
+// // bx slider 03 in top__section
+// $(document).ready(function() {
+//     $('#top__slider-03').bxSlider({
+//         mode: 'fade',
+//         buildPager: function(slideIndex) {
+//             switch (slideIndex) {
+//                 case 0:
+//                     return 'дизайн';
+//                 case 1:
+//                     return 'одностраничный сайт';
+//                 case 2:
+//                     return 'корпоративный сайт';
+//                 case 3:
+//                     return 'сайт-каталог';
+//                 case 4:
+//                     return 'интернет-магазин';
+//             }
+//         }
+//     });
+// });
 
 
-// bx slider 04 in top__section
-$(document).ready(function() {
-    $('#top__slider-04').bxSlider({
-        mode: 'fade',
-        buildPager: function(slideIndex) {
-            switch (slideIndex) {
-                case 0:
-                    return 'поддержка';
-                case 1:
-                    return 'администрирование';
-                case 2:
-                    return 'корпоративный сайт';
-                case 3:
-                    return 'сайт-каталог';
-                case 4:
-                    return 'интернет-магазин';
-            }
-        }
-    });
-});
+// // bx slider 04 in top__section
+// $(document).ready(function() {
+//     $('#top__slider-04').bxSlider({
+//         mode: 'fade',
+//         buildPager: function(slideIndex) {
+//             switch (slideIndex) {
+//                 case 0:
+//                     return 'поддержка';
+//                 case 1:
+//                     return 'администрирование';
+//                 case 2:
+//                     return 'корпоративный сайт';
+//                 case 3:
+//                     return 'сайт-каталог';
+//                 case 4:
+//                     return 'интернет-магазин';
+//             }
+//         }
+//     });
+// });
 
 
 // tabs in rocket__section
